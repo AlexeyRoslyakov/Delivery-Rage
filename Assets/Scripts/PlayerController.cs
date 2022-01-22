40604202918940816bd1636a4ae7b640e13c2db3
AlexeyRoslyakov <Advs.air@gmail.com>
2022-01-22 19:51:13 +0300

    v0.2
    Added a test scene
    Added  AI Vehicle Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool isMotor; //is this wheel attached to motor?
+    public bool isSteering; // does this wheel apply steer angle?
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+    public List<AxleInfo> axleInfos; //the information about each individual axle
+    public float maxMotorTorque; // maximum torque  the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float rpm;
+
+
+
+    private Rigidbody playerRb;
+    public Vector3 COM;
+    
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = COM;
+        
+    }
+
+    // find the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        speed = Mathf.Round(playerRb.velocity.magnitude * 3.6f);
+        //speedometerText.SetText("Speed: " + speed + " kph");
+
+        rpm = (speed % 30) * 40;
+        //rpmText.SetText("RPM: " + rpm);
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.isSteering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.isMotor )
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class AIAxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool isMotor; //is this wheel attached to motor?
+    public bool isSteering; // does this wheel apply steer angle?
+}
+
+public class VehicleController : MonoBehaviour
+{
+
+    public List<AIAxleInfo> axleInfos; //the information about each individual axle
+    public float maxMotorTorque; // maximum torque  the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    private float goingForward = 1;  // 1 make gas on vehicle, 0 no gas
+    private float turnCar;
+
+
+    [SerializeField] private float speed;
+    [SerializeField] private float rpm;
+
+
+
+    private Rigidbody vehicleRb;
+    public Vector3 COM;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        vehicleRb = GetComponent<Rigidbody>();
+        vehicleRb.centerOfMass = COM;
+
+    }
+
+    // find the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float motor = maxMotorTorque * goingForward;
+        float steering = maxSteeringAngle * turnCar;
+
+        speed = Mathf.Round(vehicleRb.velocity.magnitude * 3.6f);
+        //speedometerText.SetText("Speed: " + speed + " kph");
+
+        rpm = (speed % 30) * 40;
+        //rpmText.SetText("RPM: " + rpm);
+
+        foreach (AIAxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.isSteering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.isMotor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+
+    }
+}
